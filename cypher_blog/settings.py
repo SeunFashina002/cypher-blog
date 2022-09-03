c2938480936c92cb0f1f3c5132b12ba51abe4d63
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-09-02 23:42:14 -0700

    commented email details

--- cypher_blog/settings.py
+++ cypher_blog/settings.py
@@ -126,9 +126,9 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fashinaoluwaseun36@gmail.com'
-EMAIL_HOST_PASSWORD = 'icyDIAMOND'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'fashinaoluwaseun36@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
